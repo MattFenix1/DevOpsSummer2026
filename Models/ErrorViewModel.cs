ea05d4f801bacf8c48b4d36721b76ae780aede77
MattFenix1 <bjduffy@email.neit.edu>
2026-08-02 12:10:02 -0400

    Front-End Update 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevOpsSummer2026.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
